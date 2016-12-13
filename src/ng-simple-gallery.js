dacd783b8037e11913efae5ba92cb965270c32ef
linuser <linuser@unix.local>
2016-12-13 23:13:44 +0100

    Refactoring

--- src/ng-simple-gallery.js
+++ src/ng-simple-gallery.js
@@ -1,5 +1,5 @@
 angular
-    .module('ng-simple-gallery', [])
+    .module('ngSimpleGallery', [])
     .directive('ngGallery', ['$timeout', '$animate', function ($timeout, $animate) {
         return {
             restrict: 'AE',
@@ -20,7 +20,7 @@ angular
                         scope.animationClassNext = 'slide-left';
                         scope.animationClassPrev = 'slide-right';
                         break;
-                    default: 
+                    default: // fade
                         scope.animationClassNext = 'fade';
                         scope.animationClassPrev = 'fade';
                 }
